61f3d4afdb15d83495c88ef026e819278ceb7a3b
Noelngerem <e.ngerem@alustudent.com>
2023-06-13 19:31:24 +0100

    WEBSITE

--- Versatility.js
+++ Versatility.js
@@ -6,3 +6,20 @@ function voiceTalk () {
 }
 
 window.onload = voiceTalk();
+
+function emailUs() {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "duenn1357@gmail.com",
+        Password : "Noelngerem2004*",
+        To : 'emmanuelngerem05@gmail.com',
+        From : document.getElementById('email').value,
+        Subject : "WELLNESS AVAIL",
+        Body : "First name:" + document.getElementById("first-name").value
+            + "<br> Last name:" + document.getElementById("last-name").value
+            + "<br> Email:" + document.getElementById("email").value
+            + "<br> Reach Out Info:" + document.getElementById("reach-out").value 
+    }).then(
+        alert("Reach Out received Successfully")
+    );
+}
